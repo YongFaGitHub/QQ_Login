fc12651bfef95a9d9f7ad849b5931462179e7c91
NIMO <laomms@gmail.com>
2020-10-23 19:47:02 +0700

    modify

--- QQ_Login/Form1.Designer.cs
+++ QQ_Login/Form1.Designer.cs
@@ -28,116 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.txtUseraccount = new System.Windows.Forms.TextBox();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // webBrowser2
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(5, 7);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(416, 406);
-            this.tabControl1.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.button1);
-            this.tabPage1.Controls.Add(this.txtPassword);
-            this.tabPage1.Controls.Add(this.txtUseraccount);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(408, 380);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "账号登录";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(144, 241);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 47);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "登录";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(64, 155);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(267, 26);
-            this.txtPassword.TabIndex = 1;
-            // 
-            // txtUseraccount
-            // 
-            this.txtUseraccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUseraccount.Location = new System.Drawing.Point(64, 74);
-            this.txtUseraccount.Name = "txtUseraccount";
-            this.txtUseraccount.Size = new System.Drawing.Size(267, 26);
-            this.txtUseraccount.TabIndex = 0;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.webBrowser1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(408, 380);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "快速登录";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(3, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.ScrollBarsEnabled = false;
-            this.webBrowser1.Size = new System.Drawing.Size(400, 374);
-            this.webBrowser1.TabIndex = 1;
+            this.webBrowser2.Location = new System.Drawing.Point(12, 4);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScrollBarsEnabled = false;
+            this.webBrowser2.Size = new System.Drawing.Size(546, 419);
+            this.webBrowser2.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 418);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(570, 435);
+            this.Controls.Add(this.webBrowser2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QQ登录";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.TextBox txtUseraccount;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
     }
 }
 
--- QQ_Login/Form1.cs
+++ QQ_Login/Form1.cs
@@ -28,127 +28,173 @@ namespace QQ_Login
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Random rand = new Random();
-            double r = rand.NextDouble();
-            string codeimageurl = string.Format(@"https://ssl.ptlogin2.qq.com/ptqrshow?appid=1006102&amp;e=2&amp;l=M&amp;s=3&amp;d=72&amp;v=4&amp;t={0}&amp;daid=1&amp;pt_3rd_aid=0", r);
-            //var request = WebRequest.Create(codeimageurl);
-            //using (var response = request.GetResponse())
-            //using (var stream = response.GetResponseStream())
-            //{
-            //    pictureBox1.Image = Bitmap.FromStream(stream);
-            //}
 
-            WebBrowser browser =new WebBrowser();
-            InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
-            browser.ScriptErrorsSuppressed = false;
-            browser.Navigate(new Uri("https://id.qq.com/login/ptlogin.html"));
-            browser.DocumentCompleted += OnDomContentLoaded;
+            //WebBrowser browser =new WebBrowser();
+            //InternetSetOption(IntPtr.Zero, INTERNET_OPTION_END_BROWSER_SESSION, IntPtr.Zero, 0);
+            //browser.ScriptErrorsSuppressed = false;
+            //browser.Navigate(new Uri("https://id.qq.com/login/ptlogin.html"));
+            //browser.DocumentCompleted += browserOnLoaded;
+
+            WebBrowser mbrowser = new WebBrowser();
+            mbrowser.ScriptErrorsSuppressed = false;
+            mbrowser.Navigate(new Uri("https://im.qq.com/mobileqq/"));
+            mbrowser.DocumentCompleted += mbrowserOnLoaded;
 
         }
 
-        public static bool IsLogin(string qqnum, string code, string passwords, CookieContainer mycookiecontainer)
-        {
-            WebHeaderCollection myWebHeaderCollection = new WebHeaderCollection();
-            var redirect_geturl = string.Empty;
-            var head1 = new WebHeaderCollection();
-            Dictionary<string, string> Headerdics = new Dictionary<string, string>()
-            {
-                {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"},
-                {"ContentType", "application/x-www-form-urlencoded"},
-                {"Referer", "http://ui.ptlogin2.qq.com/cgi-bin/login?appid=1006102&s_url=http://id.qq.com/index.html"},
-                {"UserAgent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36"}
-            };
-            string password = PasswordHelper.GetPassword(qqnum, passwords, code);
-            string loginUrlstring = @"http://ptlogin2.qq.com/login?u=" + qqnum + "&p=" + password + "&verifycode=" + code + "&aid=1006102&u1=http%3A%2F%2Fid.qq.com%2Findex.html&h=1&ptredirect=1&ptlang=2052&from_ui=1&dumy=&fp=loginerroralert&action=8-29-82478035&mibao_css=&t=1&g=1";
-            string retString = HttpHelper.RequestGet(loginUrlstring, Headerdics, head1, mycookiecontainer, ref redirect_geturl);
-            Debug.WriteLine(retString);
-            return retString.Contains("ptuiCB('0',") ? true : false;
-        }
-        public static string GetVerfiyCode(string qqnum, CookieContainer mycookiecontainer)
-        {
-            WebHeaderCollection myWebHeaderCollection = new WebHeaderCollection();
-            var redirect_geturl = string.Empty;
-            var head1 = new WebHeaderCollection();
-            Dictionary<string, string> Headerdics = new Dictionary<string, string>()
-            {
-                {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"},
-                {"ContentType", "application/x-www-form-urlencoded"},
-                {"Referer", "http://ui.ptlogin2.qq.com/cgi-bin/login?appid=1006102&s_url=http://id.qq.com/index.html"},
-                {"UserAgent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36"}
-            };
-            Random rand = new Random();
-            double r = rand.NextDouble();
-            string checkcodeurl = string.Format(@"http://check.ptlogin2.qq.com/check?uin={0}&appid=1006102&r={1}", qqnum, r);
-            string retString = HttpHelper.RequestGet(checkcodeurl, Headerdics, head1, mycookiecontainer, ref redirect_geturl);
-            return retString;
-        }
+        //public static bool IsLogin(string qqnum, string code, string passwords, CookieContainer mycookiecontainer)
+        //{
+        //    WebHeaderCollection myWebHeaderCollection = new WebHeaderCollection();
+        //    var redirect_geturl = string.Empty;
+        //    var head1 = new WebHeaderCollection();
+        //    Dictionary<string, string> Headerdics = new Dictionary<string, string>()
+        //    {
+        //        {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"},
+        //        {"ContentType", "application/x-www-form-urlencoded"},
+        //        {"Referer", "http://ui.ptlogin2.qq.com/cgi-bin/login?appid=1006102&s_url=http://id.qq.com/index.html"},
+        //        {"UserAgent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36"}
+        //    };
+        //    string password = PasswordHelper.GetPassword(qqnum, passwords, code);
+        //    string loginUrlstring = @"http://ptlogin2.qq.com/login?u=" + qqnum + "&p=" + password + "&verifycode=" + code + "&aid=1006102&u1=http%3A%2F%2Fid.qq.com%2Findex.html&h=1&ptredirect=1&ptlang=2052&from_ui=1&dumy=&fp=loginerroralert&action=8-29-82478035&mibao_css=&t=1&g=1";
+        //    string retString = HttpHelper.RequestGet(loginUrlstring, Headerdics, head1, mycookiecontainer, ref redirect_geturl);
+        //    Debug.WriteLine(retString);
+        //    return retString.Contains("ptuiCB('0',") ? true : false;
+        //}
+        //public static string GetVerfiyCode(string qqnum, CookieContainer mycookiecontainer)
+        //{
+        //    WebHeaderCollection myWebHeaderCollection = new WebHeaderCollection();
+        //    var redirect_geturl = string.Empty;
+        //    var head1 = new WebHeaderCollection();
+        //    Dictionary<string, string> Headerdics = new Dictionary<string, string>()
+        //    {
+        //        {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"},
+        //        {"ContentType", "application/x-www-form-urlencoded"},
+        //        {"Referer", "http://ui.ptlogin2.qq.com/cgi-bin/login?appid=1006102&s_url=http://id.qq.com/index.html"},
+        //        {"UserAgent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36"}
+        //    };
+        //    Random rand = new Random();
+        //    double r = rand.NextDouble();
+        //    string checkcodeurl = string.Format(@"http://check.ptlogin2.qq.com/check?uin={0}&appid=1006102&r={1}", qqnum, r);
+        //    string retString = HttpHelper.RequestGet(checkcodeurl, Headerdics, head1, mycookiecontainer, ref redirect_geturl);
+        //    return retString;
+        //}
     
-        public static Stream GetVerfycodeImage(CookieContainer mycookiecontainer, string qqnum)
-        {
-            Random rand = new Random();
-            double r = rand.NextDouble();
-            string codeimageurl = string.Format("http://captcha.qq.com/getimage?aid=1006102&r={0}&uin={1}", r, qqnum);
-            Stream retStream = HttpHelper.GetStream(codeimageurl, mycookiecontainer);
-            return retStream;
-        }
+        //public static Stream GetVerfycodeImage(CookieContainer mycookiecontainer, string qqnum)
+        //{
+        //    Random rand = new Random();
+        //    double r = rand.NextDouble();
+        //    string codeimageurl = string.Format("http://captcha.qq.com/getimage?aid=1006102&r={0}&uin={1}", r, qqnum);
+        //    Stream retStream = HttpHelper.GetStream(codeimageurl, mycookiecontainer);
+        //    return retStream;
+        //}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            CookieContainer mycookiecontainer = new CookieContainer();
-            //bool success = IsLogin(this.txtUseraccount.Text, code, this.txtPassword.Text, cookie);
-            string retString = GetVerfiyCode(txtUseraccount.Text, mycookiecontainer);
-            if (retString.Contains("ptui_checkVC('0','"))
-            {
-                //不需要手动输入
-                Form frm = new Form2();
-                Form2.MyInstance.txtverfiycode.Text = retString.Replace("ptui_checkVC('0','", "").Replace("'", "").Replace(")", "").Replace(";", "").Substring(0, 4);
-                frm.ShowDialog();
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    CookieContainer mycookiecontainer = new CookieContainer();
+        //    //bool success = IsLogin(this.txtUseraccount.Text, code, this.txtPassword.Text, cookie);
+        //    string retString = GetVerfiyCode(txtUseraccount.Text, mycookiecontainer);
+        //    if (retString.Contains("ptui_checkVC('0','"))
+        //    {
+        //        //不需要手动输入
+        //        Form frm = new Form2();
+        //        Form2.MyInstance.txtverfiycode.Text = retString.Replace("ptui_checkVC('0','", "").Replace("'", "").Replace(")", "").Replace(";", "").Substring(0, 4);
+        //        frm.ShowDialog();
 
-            }
-            else if (retString.Contains("ptui_checkVC('1',"))
-            {
-                //需要手动输入，将验证码输出在image中
-                Form frm = new Form2();
-                Form2.MyInstance.vefycodpicbox.Image = Image.FromStream(GetVerfycodeImage(mycookiecontainer, this.txtUseraccount.Text));
-                frm.ShowDialog();                
-            }
-            bool success = IsLogin(txtUseraccount.Text,VerificationCode,txtPassword.Text,mycookiecontainer);
-            if (success)
-            {
-                MessageBox.Show("登录成功!");
-            }
-            else
-            {
-                MessageBox.Show("登录失败!");
-            }
-        }
+        //    }
+        //    else if (retString.Contains("ptui_checkVC('1',"))
+        //    {
+        //        //需要手动输入，将验证码输出在image中
+        //        Form frm = new Form2();
+        //        Form2.MyInstance.vefycodpicbox.Image = Image.FromStream(GetVerfycodeImage(mycookiecontainer, this.txtUseraccount.Text));
+        //        frm.ShowDialog();                
+        //    }
+        //    bool success = IsLogin(txtUseraccount.Text,VerificationCode,txtPassword.Text,mycookiecontainer);
+        //    if (success)
+        //    {
+        //        MessageBox.Show("登录成功!");
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("登录失败!");
+        //    }
+        //}
 
-        public void OnDomContentLoaded(object sender, EventArgs e)
+        //public void browserOnLoaded(object sender, EventArgs e)
+        //{
+        //    var browser = (WebBrowser)sender;
+        //    if (browser != null && browser.Document != null && browser.Document.Body != null)
+        //    {
+        //        IHTMLDocument2 ihtmldocument = (IHTMLDocument2)browser.Document.DomDocument;
+        //        HtmlElementCollection elementsByTagName = browser.Document.GetElementsByTagName("IFRAME");
+        //        if (elementsByTagName.Count > 0)
+        //        {
+        //            foreach (HtmlElement htmlElement in elementsByTagName)
+        //            {
+        //                if (htmlElement.OuterHtml.Contains("xui.ptlogin2.qq.com/cgi-bin/xlogin?"))
+        //                {
+        //                    var source = "https:" + Regex.Match(htmlElement.OuterHtml, "<iframe.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
+        //                    string _embeddedpage = @"<html> <body><iframe class='login_frame' type='text/html' width='" + (webBrowser1.Width-20).ToString() + "' height='305' src='"+ source + "' frameborder='0'></iframe></body><html>";
+        //                    webBrowser1.DocumentText = _embeddedpage;
+        //                    webBrowser1.ScriptErrorsSuppressed = true;
+        //                    webBrowser1.DocumentCompleted += webBrowser1Loaded;
+
+        //                }
+                       
+        //            }
+        //        }
+         
+        //    }
+        //}
+
+        public void mbrowserOnLoaded(object sender, EventArgs e)
         {
+            webBrowser2.Navigate(new Uri("about:blank"));
             var browser = (WebBrowser)sender;
             if (browser != null && browser.Document != null && browser.Document.Body != null)
             {
-                IHTMLDocument2 ihtmldocument = (IHTMLDocument2)browser.Document.DomDocument;
-                HtmlElementCollection elementsByTagName = browser.Document.GetElementsByTagName("IFRAME");
-                if (elementsByTagName.Count > 0)
+                if (browser.Document.Body.InnerHtml.IndexOf("web_login", StringComparison.OrdinalIgnoreCase) + 1 > 0)
                 {
-                    foreach (HtmlElement htmlElement in elementsByTagName)
+                    var bt = browser.Document.GetElementById("web_login");
+                }
+                else if (browser.Document.Body.InnerHtml.IndexOf("switcher_plogin", StringComparison.OrdinalIgnoreCase) + 1 > 0)
+                {
+                    browser.Document.GetElementById("login").InvokeMember("click");
+                }
+                else if (browser.Document.Body.InnerHtml.IndexOf("快速安全登录", StringComparison.OrdinalIgnoreCase) + 1 > 0)
+                {
+                    browser.Document.GetElementById("login").InvokeMember("click");
+                }
+                else if (browser.Document.Body.InnerHtml.IndexOf("login_frame", StringComparison.OrdinalIgnoreCase) + 1 > 0)
+                {
+                    IHTMLDocument2 ihtmldocument = (IHTMLDocument2)browser.Document.DomDocument;
+                    HtmlElementCollection elementsByTagName = browser.Document.GetElementsByTagName("iframe");
+                    if (elementsByTagName.Count > 0)
                     {
-                        if (htmlElement.OuterHtml.Contains("xui.ptlogin2.qq.com/cgi-bin/xlogin?"))
+                        foreach (HtmlElement htmlElement in elementsByTagName)
                         {
-                            var source = "https:" + Regex.Match(htmlElement.OuterHtml, "<iframe.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value;
-                            string _embeddedpage = @"<html> <body><iframe class='login_frame' type='text/html' width='" + (webBrowser1.Width-20).ToString() + "' height='305'src='"+ source + "' frameborder='0'></iframe></body><html>";
-                            webBrowser1.DocumentText = _embeddedpage;
-                            webBrowser1.ScriptErrorsSuppressed = true;
-                            webBrowser1.DocumentCompleted += webBrowser1Loaded;
+                            if (htmlElement.OuterHtml.Contains("https://xui.ptlogin2.qq.com/cgi-bin/xlogin?appid="))
+                            {
+                                var source =  Regex.Match(htmlElement.OuterHtml, "<iframe.+?src=[\"'](.+?)[\"'].*?>", RegexOptions.IgnoreCase).Groups[1].Value.Replace("target=self&amp;", "target=_new&amp;");
+                                string _embeddedpage = @"<html> <body><iframe class='login_frame' type='text/html' width='" + (webBrowser2.Width - 20).ToString() + "' height='" + (webBrowser2.Height - 20).ToString() + "' src='" + source + "'></iframe></body><html>";
+                                webBrowser2.DocumentText = _embeddedpage;
+                                webBrowser2.ScriptErrorsSuppressed = true;
+                                webBrowser2.DocumentCompleted += webBrowser1Loaded;
+
+                                break;
+                            }
+
                         }
-                       
                     }
                 }
-         
+                else if (browser.Document.Body.InnerHtml.IndexOf("登录", StringComparison.OrdinalIgnoreCase) + 1 > 0)
+                {
+                    browser.Document.GetElementById("login").Focus();
+                    browser.Document.GetElementById("login").InvokeMember("Click");
+                    //SendKeys.Send("{ENTER}");
+                }  
+
             }
         }
-
         public void webBrowser1Loaded(object sender, EventArgs e)
         {
             var browser = (WebBrowser)sender;
@@ -158,9 +204,9 @@ namespace QQ_Login
                 if (string.IsNullOrEmpty(cookieStr)==false)
                 {
                     string[] cookstr = cookieStr.Split(';');
-                    foreach (string str in cookstr)
+                    foreach (string cookie in cookstr)
                     {
-                        Debug.WriteLine(str);
+                        Debug.WriteLine(cookie);
                     }
                 }
               
